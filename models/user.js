73df51188334381f39c7593198dfeb00388a775e
Silpa <priyadarshini.silpa13@gmail.com>
2022-08-01 00:37:51 +0530

    minor changes

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
-    email : { type: String, 
-              required: true, 
-              unique: true,
-              //match: //\S+@\S+\.\S+//
-            },
-    password : { type: String, required: true}
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
